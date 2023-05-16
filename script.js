dc478d9af2b5fc7b32e521d6f44a36ec7db233c4
Ugur Alp Yildiz <77773407+uguralpyildiz@users.noreply.github.com>
2023-05-17 02:55:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,94 @@
+// Metamask bağlantı durumunu localStorage'da saklamak için anahtar
+const storageKey = 'metamaskConnected';
+
+// Sayfa yüklendiğinde bağlantı durumunu kontrol et
+if (localStorage.getItem(storageKey) === 'true') {
+    document.addEventListener('DOMContentLoaded', async () => {
+        const isConnected = localStorage.getItem(storageKey) === 'true';
+        if (isConnected) {
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            if (accounts.length > 0) {
+                showMetamaskInfo();
+                const metamaskButton = document.getElementById('metamaskButton');
+                metamaskButton.innerHTML = "Disconnect"
+                metamaskButton.style.background = "#905F23"
+            }
+        }
+    });
+}
+
+// Metamask butonuna tıklandığında çağrılacak fonksiyon
+async function openMetamask() {
+    // Metamask'ın var olup olmadığını kontrol et
+    if (typeof window.ethereum !== 'undefined') {
+        // Metamask'ı açmak için istemeyi gönder
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+        // Kullanıcının giriş yapması durumunda alınan hesap bilgilerini göster
+        if (accounts.length > 0) {
+            const address = accounts[0];
+            const metamaskButton = document.getElementById('metamaskButton');
+            metamaskButton.innerHTML = "Disconnect";
+            metamaskButton.style.background = "#905F23";
+
+            // Hesap bakiyesini al
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const balance = await provider.getBalance(address);
+
+            // Bakiyeyi göster
+            const balanceInfoDiv = document.getElementById('balanceInfo');
+            const etherBalance = ethers.utils.formatEther(balance);
+
+            // Bağlantı durumunu localStorage'a kaydet
+            if (metamaskButton.innerHTML === "Disconnect") {
+                localStorage.setItem(storageKey, 'true');
+            }
+        }
+    } else {
+        // Metamask bulunamazsa kullanıcıya bir hata mesajı göster
+        alert('Metamask uygulaması bulunamadı. Lütfen yükleyin ve giriş yapın.');
+    }
+}
+
+// Metamask bağlantı durunda kullanıcı bilgilerini gösteren fonksiyon
+async function showMetamaskInfo() {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const address = accounts[0];
+    const infoDiv = document.getElementById('metamaskInfo');
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const balance = await provider.getBalance(address);
+
+    const balanceInfoDiv = document.getElementById('balanceInfo');
+    const etherBalance = ethers.utils.formatEther(balance);
+}
+
+// Metamask çıkış fonksiyonu
+function logoutMetamask() {
+    const metamaskButton = document.getElementById('metamaskButton');
+    metamaskButton.innerHTML = "Connect";
+    metamaskButton.style.background = "";
+    localStorage.removeItem(storageKey);
+}
+
+// Metamask butonuna tıklanınca openMetamask fonksiyonunu çağır
+const metamaskButton = document.getElementById('metamaskButton');
+metamaskButton.addEventListener('click', function () {
+    if (metamaskButton.innerHTML === "Disconnect") {
+        logoutMetamask();
+    } else {
+        openMetamask();
+    }
+});
+
+setTimeout(() => {
+    if (metamaskButton.innerHTML === "Connect") {
+        localStorage.removeItem(storageKey);
+    }
+}, 1500);         
+    
+
+
+
 const values = document.querySelectorAll(".values")
 const xmark = document.querySelectorAll(".xmark")
 
@@ -20,7 +111,22 @@ for (let i = 0; i < values.length; i++) {
 
 
 
+const submitBtn = document.querySelector(".submit-btn")
+const forms = document.querySelector("form")
+const emailV = document.querySelector("#email")
+const subjectV = document.querySelector("#subject")
 
+forms.addEventListener("submit", ()=>{
+    submitBtn.value = "Sending..."
+    setTimeout(() => {
+        emailV.value = ""
+        subjectV.value = ""
+        submitBtn.value = "Thank You"
+    }, 1200);
+    setTimeout(() => {
+        submitBtn.value = "Send"
+    }, 3200);
+})
 
 
 const nthAll = document.querySelectorAll("#nth a");
